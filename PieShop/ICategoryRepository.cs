8c8cd1976c6fbd49f8d6da8eb6e66b089b975472
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2022-11-27 18:44:00 +0100

    4. Creating the Model and the Repository

---
+++ PieShop/ICategoryRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace PieShop
+{
+    public interface ICategoryRepository
+    {
+    }
+}
---
+++ PieShop/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace PieShop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = String.Empty;
+        public string? Description { get; set; }
+        public List<Pie>? Pies { get; set; }
+    }
+}
