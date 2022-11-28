15aa32d7b55dd5b1b23765ac1fbb8eac1948bd44
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2022-11-28 16:46:54 +0100

    5. Adding EF core and creating the Repositories

---
+++ PieShop/Models/PieRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly PieShopDbContext pieShopContext;
+
+        public PieRepository(PieShopDbContext dbContext)
+        {
+            this.pieShopContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get 
+            {
+                return pieShopContext.Pies.Include(c => c.Category); 
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return pieShopContext.Pies.Include(c => c.Category)
+                                          .Where(p => p.IsPieOfTheWeek); 
+            }
+        }
+
+        public Pie? GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -1,10 +1,17 @@
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 using PieShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+builder.Services.AddDbContext<PieShopDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:PieShopDbContextConnection"]);
+});
 
 builder.Services.AddControllersWithViews();
 
